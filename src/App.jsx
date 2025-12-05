cb653ee0d1dca21008ab7ddf13516b1bb3d5407a
ANTHNO <anthonyppbittencourt@gmail.com>
2025-12-05 11:50:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import s from './App.module.css'
 import tresLinhas from '../public/hamburger.png'
 import menuLOGO from '../public/mcLOGO.png'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+
+  
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsTop(window.scrollY === 0);
+    }
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
 
   return (
     <>
       <main>
-        <div className={s.menu}>
+        <nav className={`${s.menu} ${!isTop ? s.shrink : ""}`}>
           <div className={s.opcaoMenu}>
             <img height={60} width={s.auto} src={tresLinhas}/>
             <img height={95} width={s.auto} src={menuLOGO}/>
@@ -19,7 +36,7 @@ function App() {
             </div>
           </div>
 
-        </div>
+        </nav>
         <div className={s.s1}>
 
 
